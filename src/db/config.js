5f3255f9d61757fc3a51b22e2210b1dfe0b6895a
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2024-07-09 08:49:37 +0530

    string update

--- src/db/config.js
+++ src/db/config.js
@@ -1,11 +1,6 @@
-// const path = require('path');
-// require("dotenv").config({ path: path.resolve(".env") });
+
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://127.0.0.1:27017/result_generator_db');
-// mongoose.connect(`mongodb+srv://bhoopendrasingh2306:bhoopendra@workmanagement.6gzxm1u.mongodb.net/?retryWrites=true&w=majority&appName=workmanagement`);
-// const uri =process.env.CONNECTION_URI;
-console.log(process.env.CONNECTION_URI)
-mongoose.connect(process.env.CONNECTION_URI) ;
+mongoose.connect(`mongodb+srv://bhoopendrasingh2306:bhoopendra@cluster0.cng1qah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
 mongoose.connection.once('open', () => {
     console.log('mongodb connected successfully',);
 });
