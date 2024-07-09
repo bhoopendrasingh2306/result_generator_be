9cce3e579b8997809ee60fea07b8a1e761362f72
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2024-07-09 08:42:41 +0530

    db

--- src/db/config.js
+++ src/db/config.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/result_generator_db');
 // mongoose.connect(`mongodb+srv://bhoopendrasingh2306:bhoopendra@workmanagement.6gzxm1u.mongodb.net/?retryWrites=true&w=majority&appName=workmanagement`);
 // const uri =process.env.CONNECTION_URI;
+console.log(process.env.CONNECTION_URI)
 mongoose.connect(process.env.CONNECTION_URI) ;
 mongoose.connection.once('open', () => {
     console.log('mongodb connected successfully',);
