5502e73c28745d03c1c53473cbc0daf1b29b7155
Bhoopendra Singh <bhoopendrasingh2306@gmail.com>
2024-12-11 17:06:17 +0530

    added payment gateway

--- src/app.js
+++ src/app.js
@@ -2,21 +2,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const fileUpload = require("express-fileupload");
-var bodyParser = require("body-parser");
-
 require("dotenv").config();
+const stripe = require("stripe")(process.env.SECRET_KEY_STRIPE);
+const { v4: uuidv4 } = require('uuid');
+
+
 require("./db/config");
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(
-  fileUpload({
-    useTempFiles: true,
-  })
-);
+
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 app.use(
   cors({
     origin: "*",
@@ -35,6 +30,37 @@ app.use("/university", university);
 
 const result = require("./routes/result_routes");
 app.use("/result", result);
+
+app.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("product", product);
+  console.log("price", product.price);
+
+  const idempotencyKey = uuidv4();
+
+  return stripe.customers.create({
+    email: token.email,
+    source: token.id,
+  }).then(customer => {
+    // Ensure idempotency_key is correctly passed as part of the options object
+    return stripe.charges.create(
+      {
+        amount: product.price * 100, // Amount in cents
+        currency: "usd",
+        customer: customer.id,
+        description: "Sample Charge",
+        receipt_email: token.email,
+      }
+    );
+  }).then(result => {
+    res.status(200).json(result);
+  }).catch(error => {
+    console.log("backend error", error);
+    res.status(500).json({ error: "Payment processing failed" });
+  });
+});
+
+
 
 app.listen(process.env.PORT || 5000, function () {
   console.log("App running on port 4000.");
